3407680941a19b3855762750126e852be1fffbcf
Estelle Dose <estelle.dose@laplateforme.io>
2023-10-25 09:49:09 +0200

    Title

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from './Title';
+
+function App() {
+  return (
+    <div>
+      <Title />
+      {/* Autres éléments de votre application */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <h1>Calculator9000</h1>
+  );
+}
+
+export default Title;
